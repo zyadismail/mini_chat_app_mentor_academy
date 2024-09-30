7ea7aded2a7d7af8ef2c94399d5f752fd6d2022c
zyad ismail <ziadismail949@gmail.com>
2024-09-30 14:22:38 +0300

    Initial commit on dev branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(){
   runApp(const MyApp());
@@ -10,6 +11,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green,),
+        textTheme: GoogleFonts.interTextTheme(),
+        useMaterial3: true,
+      )
+    );
   }
 }
